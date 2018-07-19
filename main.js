b9d6c05602c1626f5ed7c2281a0d953e86011590
Helen <helen@Helens-MacBook-Pro.local>
2018-07-19 15:28:19 -0500

    saving to local storage

--- main.js
+++ main.js
@@ -20,14 +20,53 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("execSlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+//Testing Ground//
+
+const lilDickyInfoDatabase = {}
+
+lilDickyInfoDatabase.Personal = {
+  Title: "Personal",
+  Name: "Name: David Andrew Burd",
+  Birthdate: "Born: March 15, 1988",
+  Relationship: "Relationship Status: Not Married",
+  From: "From: Philadelphia, Pennsylvania",
+  Current: "Currently: Los Angeles, California"
+}
+
+const saveDatabase = function (databaseObject, localStorageKey) {
+  const stringifiedDatabase = JSON.stringify(databaseObject)
+  localStorage.setItem(localStorageKey, stringifiedDatabase)
+
+}
+
+saveDatabase(lilDickyInfoDatabase, "Personal")
+
+
+//Loading the Database
+
+const loadDatabase = function (localStorageKey) {
+
+  const databaseString = localStorage.getItem(localStorageKey)
+
+  return JSON.parse(databaseString)
+}
+
+const LilDicPers = loadDatabase("Personal")
+console.log(LilDicPers);
+
+const birthDate = document.createElement("p")
+birthDate.innerHTML = lilDickyInfoDatabase.Personal.Birthdate
+const container = document.getElementById("Personal")
+container.appendChild(birthDate);
