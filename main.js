8d13762705cf80be002829ba1fb08dddeb57c5ba
Akeen11 <aartkeen@ut.utm.edu>
2018-07-19 15:26:15 -0500

    tuners

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ document.getElementById("birds").onmouseover = function() {mouseOver()};
 function mouseOver(){
 
 }
+
+// function load_newsFeed() {
+//     document.getElementById("news-feed").innerHTML='<object type="text/html" data="news-feed.html"></object>';
+// }
+
+// load_newsFeed()
+
+var link = document.querySelector('link[rel="import"]');
+    var content = link.import;
+
+    // Grab DOM from warning.html's document.
+    var el = content.querySelector('#news-feed');
+    const feedDocumentLocation = document.querySelector('.location')
+
+    feedDocumentLocation.appendChild(el.cloneNode(true));
