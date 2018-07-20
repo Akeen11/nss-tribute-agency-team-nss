13a52f661bb08ae2d0a6188593afa1c36380c45e
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-07-19 21:09:16 -0500

    new master

--- execSummaryJS.js
+++ execSummaryJS.js
@@ -1,39 +1,91 @@
-const htmlData = {}
+const execSummary = {}
 
-htmlData.execSummary = []
-htmlData.career = []
-htmlData.newsFeed = []
-htmlData.Personal = []
+execSummary.title = []
+execSummary.name = []
+execSummary.country = []
+execSummary.knownAliases = []
+execSummary.kAOne = []
+execSummary.kATwo = []
+execSummary.kAThree = []
+execSummary.kAFour = []
+execSummary.knownCollaborators = []
 
-let testData =  "Executive Summary";
+let title =  "Executive Summary";
+let name =  "Lil'Dicky";
+let country =  "United States of America";
+let knownAliases =  "Known Aliases";
+let kAOne =  "Lil Dicky";
+let kATwo =  "LD";
+let kAThree =  "Brain";
+let kAFour =  "David Andrew Burd";
+let knownCollaborators =  "Known Collaborators";
 
 
-htmlData.career.push(testData);
+execSummary.title.push(title);
+execSummary.name.push(name);
+execSummary.country.push(country);
+execSummary.knownAliases.push(knownAliases);
+execSummary.kAOne.push(kAOne);
+execSummary.kATwo.push(kATwo);
+execSummary.kAThree.push(kAThree);
+execSummary.kAFour.push(kAFour);
+execSummary.knownCollaborators.push(knownCollaborators);
 
 
-const saveDatabaseExec = function (databaseObject, localStorageKey) {
-
+const saveDatabase = function (databaseObject, localStorageKey) {
+    /*
+        Convert the Object into a string.
+    */
     const stringifiedDatabase = JSON.stringify(databaseObject)
 
+    /*
+        Create a key in local storage, and store the string
+        version of your inventory database as the value
+    */
     localStorage.setItem(localStorageKey, stringifiedDatabase)
 }
 
-const loadDatabaseExec = function (localStorageKey) {
-
+const loadDatabase = function (localStorageKey) {
+    // Get the string version of the database
     const databaseString = localStorage.getItem(localStorageKey)
 
+    // Use JSON.parse() to convert the string back into an object
     return JSON.parse(databaseString)
 }
 
-saveDatabaseExec(htmlData.career, "test" );
+saveDatabase(execSummary.title, "title" );
+saveDatabase(execSummary.name, "name" );
+saveDatabase(execSummary.country, "country" );
+saveDatabase(execSummary.knownAliases, "knownAliases" );
+saveDatabase(execSummary.kAOne, "kAOne" );
+saveDatabase(execSummary.kATwo, "kATwo" );
+saveDatabase(execSummary.kAThree, "kAThree" );
+saveDatabase(execSummary.kAFour, "kAFour" );
+saveDatabase(execSummary.knownCollaborators, "knownCollaborators" );
 
 
-loadDatabaseExec("test");
+loadDatabase("title");
+loadDatabase("name");
+loadDatabase("country");
+loadDatabase("knownAliases");
+loadDatabase("kAOne");
+loadDatabase("kATwo");
+loadDatabase("kAThree");
+loadDatabase("kAFour");
+loadDatabase("knownCollaborators");
 
-const execLoop = function (exec){
-    for (i=2; i<=exec.length-3; i++){
-       let dumb = document.getElementById("title");
-        dumb.innerHTML+= exec[i];
+const execLoop = function (dataPoint, id){
+    for (i=2; i<=dataPoint.length-3; i++){
+       let dumb = document.getElementById(id);
+        dumb.innerHTML+= dataPoint[i];
     }
 }
-execLoop(localStorage.test)
+execLoop(localStorage.title, "title")
+execLoop(localStorage.name, "name")
+execLoop(localStorage.country, "country")
+execLoop(localStorage.knownAliases, "knownAliases")
+execLoop(localStorage.kAOne, "kAOne")
+execLoop(localStorage.kATwo, "kATwo")
+execLoop(localStorage.kAThree, "kAThree")
+execLoop(localStorage.kAFour, "kAFour")
+execLoop(localStorage.knownCollaborators, "knownCollaborators")
